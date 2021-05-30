466518aa18b5299a64ea39307c09e1b12193641d
i434633 <i434633@fhict.nl>
2021-05-30 15:25:29 +0200

    Fixed get all function and added get function that sorts alphabetically

--- src/main/java/com/school/ingredientsservice/service/IngredientService.java
+++ src/main/java/com/school/ingredientsservice/service/IngredientService.java
@@ -3,6 +3,7 @@ package com.school.ingredientsservice.service;
 import com.school.ingredientsservice.entity.Ingredient;
 import com.school.ingredientsservice.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,11 +14,18 @@ public class IngredientService {
     @Autowired
     private IngredientRepository ingredientRepository;
 
+    public List<Ingredient> getAllIngredientsInOrder() {
+        return ingredientRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+    }
     public List<Ingredient> getAllIngredients() {
         return ingredientRepository.findAll();
     }
 
     public Ingredient saveIngredient(Ingredient ingredient) {
         return ingredientRepository.save(ingredient);
+    }
+
+    public void deleteIngredientById(Long ingredientId) {
+        ingredientRepository.deleteById(ingredientId);
     }
 }
