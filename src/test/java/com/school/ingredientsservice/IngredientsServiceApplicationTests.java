2561bc71651ced4ac3e9f7e464c92f38f15c0546
Parwani,Mohammad M.S <m.parwani@student.fontys.nl>
2021-05-30 21:29:14 +0200

    Getting date from 2 services work. Getting the amount of a ingredient
    MErge

---
+++ src/test/java/com/school/ingredientsservice/IngredientsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.ingredientsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
