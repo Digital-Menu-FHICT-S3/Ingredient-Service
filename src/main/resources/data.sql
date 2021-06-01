32d30b11ab6c43d14995fa22da5974135a2c1186
i434633 <i434633@fhict.nl>
2021-06-01 09:55:53 +0200

    attempt on testing get request in ingredientController

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO INGREDIENT (INGREDIENT_ID, name, amount) VALUES
+(1L, 'Steak', 10),
+(2L, 'Rice', 100),
+(3L, 'Potatoes', 50);
--- src/test/java/com/school/ingredientsservice/controller/IngredientControllerTest.java
+++ src/test/java/com/school/ingredientsservice/controller/IngredientControllerTest.java
@@ -2,19 +2,30 @@ package com.school.ingredientsservice.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.school.ingredientsservice.entity.Ingredient;
+import com.school.ingredientsservice.service.IngredientService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.UnsupportedEncodingException;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
@@ -28,8 +39,17 @@ class IngredientControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private IngredientService ingredientService;
+
     @Test
-    void getAllIngredients() {
+    void getAllIngredients() throws Exception {
+
+         mvc.perform(get("/ingredient/all")
+                 .contentType(MediaType.APPLICATION_JSON)
+                 .accept(MediaType.APPLICATION_JSON))
+                 .andExpect(status().isOk())
+                 .andReturn();
     }
 
     @Test
